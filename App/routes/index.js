a2e4e9443b1228be2586d73f5089597a11d2718a
abhi <abhipatel8675@gmail.com>
2025-06-17 15:36:15 -0400

    discard unnecessary changes

--- App/routes/index.js
+++ App/routes/index.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 
 const router = Router();
-// require('../models/user');
 const adminRoutes = require('./admin');
 const authRoutes = require('./auth');
 const userRoutes = require('./user');
@@ -9,9 +8,6 @@ const taskHazardRoutes = require('./task_hazard.routes');
 const assetHierarchyRoutes = require('./asset_hierarchy.routes');
 const tacticRoutes = require('./tacticRoutes');
 const paymentRoutes = require('./payment.routes');
-
-// Remove this import as we're handling it directly in app.js
-// const taskHazardRoutes = require('./task_hazard.routes');
 
 router.use('/auth', authRoutes);
 router.use('/admin', adminRoutes);
@@ -21,7 +17,5 @@ router.use('/asset-hierarchy', assetHierarchyRoutes);
 router.use('/users', userRoutes);
 router.use('/tactics', tacticRoutes);
 router.use('/payments', paymentRoutes);
-// Remove this line as we're handling it directly in app.js
-// router.use('/task-hazards', taskHazardRoutes);
 
 module.exports = router;
